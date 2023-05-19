1e9209d0670d83afdfe8d58600cb3bb275bfcebb
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-19 07:32:36 +0000

    fix: いろいろ

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const { isPropertyAccessChain } = require('typescript')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         serverActions: true,
     }
+}
+
+if (process.env.CONTAINER_MODE === "true") {
+    nextConfig.output = "standalone";
 }
 
 module.exports = nextConfig
